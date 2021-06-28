909356ca28498c1cecc09704c1985e3c957659f2
Ubuntu <jaydenlsf@vm.bgwdkqchdoxurm4sm2w4owpquf.zx.internal.cloudapp.net>
2021-06-28 14:47:32 +0000

    Minor changes

--- frontend/docker.sh
+++ frontend/docker.sh
@@ -4,4 +4,4 @@
 docker build -t spring-petclinic-angular:latest .
 
 # run docker container
-docker run --rm -d -p 8080:8080 --name frontend_container jaydenlsf/spring-petclinic-angular:latest
+docker run --rm -d -p 5000:5000 --name frontend_container jaydenlsf/spring-petclinic-angular:latest
