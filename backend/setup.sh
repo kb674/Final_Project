4b9de3a9fa7649d64f9fdb56ffb1c2c448f7fb7d
Ubuntu <jaydenlsf@testVM.bgwdkqchdoxurm4sm2w4owpquf.zx.internal.cloudapp.net>
2021-06-29 17:24:28 +0000

    Minor changes

--- backend/setup.sh
+++ backend/setup.sh
@@ -7,7 +7,7 @@ sudo apt-get install maven -y
 sudo apt install mysql-client-core-5.7
 
 # run backend in a docker container
-docker run --rm -d -p 9966:9966 --name backend_container springcommunity/spring-petclinic-rest
+docker run --rm -d -p 9966:9966 --name backend jaydenlsf/spring-petclinic-rest
 
 # run mysql in a docker container
 docker run -e MYSQL_ROOT_PASSWORD=petclinic -e MYSQL_DATABASE=petclinic -d -p 3306:3306 --name mysql-petclinic mysql:5.7.8
