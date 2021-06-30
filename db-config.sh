87aeadc55ecb80bf19bf6267b2096530796e9dad
jaydenlsf <jaydenlsf47@gmail.com>
2021-06-30 09:01:02 +0000

    Removed

--- db-config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "CREATE DATABASE IF NOT EXISTS petclinic;" | mysql --host=petclinic.mysql.database.azure.com --user=petclinicdb@petclinic -p${DB_PASSWORD}
-
-mysql --host=petclinic.mysql.database.azure.com --user=petclinicdb@petclinic -e "CREATE DATABASE IF NOT EXISTS petclinic;" -p${DB_PASSWORD} 2>/dev/null
