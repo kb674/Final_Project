4095dee8afecfba5d60f027c6a783a0198d94eb5
Ubuntu <kusha@linuxy.ybkhrgze1kkutlrj12rquqs5cf.zx.internal.cloudapp.net>
2021-06-30 15:17:31 +0000

    Changed deploy.sh line 8

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -5,7 +5,7 @@ terraform apply -auto-approve
 
 cd -
 
-az aks get-credentials --resource-group k8s-test --name k8s-test-cluster
+az aks get-credentials --resource-group k8s-test --name example-project-k8s
 kubectl  apply -f ./k8s_files 
 sleep 40
 kubectl get pods
