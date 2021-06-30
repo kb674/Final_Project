dcf0c0f2769d0bc8963752dc0d556bbca5bcc90f
Ubuntu <kusha@linuxy.ybkhrgze1kkutlrj12rquqs5cf.zx.internal.cloudapp.net>
2021-06-30 14:40:27 +0000

    Deploy.sh spelling change. Updated main.tf in terraform folder.

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -5,7 +5,7 @@ terraform apply -auto-approve
 
 cd -
 
-az aks get-credentials --resource-group k8-test --name k8s-test-cluster
+az aks get-credentials --resource-group k8s-test --name k8s-test-cluster
 kubectl  apply -f ./k8s_files 
 sleep 40
 kubectl get pods
--- terraform-files/k8smodels/main.tf
+++ terraform-files/k8smodels/main.tf
@@ -1,14 +1,24 @@
 terraform {
-    required_providers {
-        azurerm = {
-            source  = "hashicorp/azurerm"
-            version = "~> 2.46.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.1"
     }
+  }
 }
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-    features {}
+  features {}
+
+  use_msi = true
+  subscription_id = "a9776c0d-e546-42a7-a002-63ba4a52ba62"
+  tenant_id = "de28047b-0625-4fad-adc8-487a110ddb70"
+}
+
+resource "azurerm_resource_group" "main" {
+    name     = "${var.project_name}-rg"
+    location = var.location
 }
 
 module "k8s" {
